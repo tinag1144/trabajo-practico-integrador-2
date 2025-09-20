2f5d82198f56ef7ce7e15b14f47e9e1f400c178d
unknown <ag444107@gmail.com>
2025-09-20 19:11:11 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors({
+    origin: `http://localhost:${PORT}`,
+    credentials: true
+}));
+app.use(cookieParser());
+
+
+app.get("/", (req, res) => {
+    res.send("Server readyyyy");
+})
+app.listen(PORT, async () => {
+    console.log(`servidor corriendo en: http://localhost:${PORT}`)
+});
